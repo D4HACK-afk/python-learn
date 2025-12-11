8886c782e135428a30a08a6f366e5f3b1bc38976
# <#>
2025-12-10 19:25:49 -0500

    se modifico strings

--- strings.py
+++ strings.py
@@ -5,4 +5,4 @@ oracion = "oracion de prueba"
 print (oracion[0:0:0])
 
 # ejemplo de como ingresar y jugar a ciertos elementos de una variable, ya sea citar especificamente palabras de un punto a otro
-#o las letras en especifico, y tambien saltarse caracteres, etc
+#o las letras en especifico, y tambien saltarse caracteres, 
