d880fd3f2ce27af32ac36bad5e548488d52a9c2b
# <d4hack2308@gmail.com>
2025-12-12 13:31:07 -0500

    ai

--- modi_list.py
+++ modi_list.py
@@ -4,9 +4,12 @@ edades = [21, 22, 23]
 aprobado = [True, False, True]
 
     #aqui aprendemos a modificar listas con funciones
+    
 nombres.append("dani")
 nombres.append("prueba de eliminacion")
 nombres.insert(0, "juanito")
 nombres.remove("prueba de eliminacion")
+
     #otra funcion es la .clear(), pero no la pongo debido a que esta elimina todo el contenido de una lista
+
 print(nombres)  
