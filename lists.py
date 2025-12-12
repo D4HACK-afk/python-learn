054e8499057d0effd63d5f327a1f32eb9775d2fa
# <d4hack2308@gmail.com>
2025-12-11 21:25:57 -0500

    aqui agregue list.py

---
+++ lists.py
@@ -0,0 +1,11 @@
+#!/usr/bin/env python3
+
+nombres = ["juan", "pablo", "daniel"]
+edades = [21, 22, 23]
+aprobado = [True, False, True]
+
+lista = [21, 22, "texto", True]
+
+#aqui se aprende a crear listas con variables y como acceder a ellas en el print#
+
+print(lista[0])
