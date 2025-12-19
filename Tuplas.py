9d15f8aa033189d8e25658e6854212f947b40c91
skeletor-H <danieltrabajo2308@gmail.com>
2025-12-18 19:02:47 -0800

    ejemplos mas concretos

--- Tuplas.py
+++ Tuplas.py
@@ -7,8 +7,12 @@ aprovados = (True, False, False)
 #modifica directamente se anade
 EdadJuan = (edades[0])
 EdadJuan += 2
+#asi es como se le puede sumar a una variable con un valor no se modifica directamente se anade, ejemplo, no queremos modificar la lista original
+#pero si queremos agregar un nuevo valor a la lista original, y para eso creamos una lista ficticia que se basa en el contenido de la original
+#por eso el contenido de la original no se modifica como en las listas comunes, esto es una principal diferencia entre las tuplas y las listas
 add_Alumnos = Alumnos 
-add_Alumnos +=  ("Pablo",)
+add_Alumnos += ("Pablo")
+
 print (Alumnos)
 print (EdadJuan)
 print (add_Alumnos [3])
