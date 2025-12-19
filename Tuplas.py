837d7bc2912e99a697228dbf609b1d33c006243f
Pablitoh <Pablitoh>
2025-12-18 21:34:01 -0500

    Esto es un ejercio de las Tuplas

---
+++ Tuplas.py
@@ -0,0 +1,13 @@
+#!/usr/bin/env python3
+Alumnos = ("Juan", "Maria", "jose")
+edades = (25, 30, 20)
+aprovados = (True, False, False)
+
+#Asi es como se le le puede sumar o multiplicar depende de lo que quieras hacer a una variable con un valor no se 
+#modifica directamente se anade
+EdadJuan = (edades[0])
+EdadJuan += 2
+
+Alumnos.insert (0, "Pablo")
+print (Alumnos)
+print (EdadJuan)
